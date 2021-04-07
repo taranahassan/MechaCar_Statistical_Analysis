e016d5e4c114bcc2710172d79a6c1d6a2aed10ac
Tarana <75437852+taranahassan@users.noreply.github.com>
2021-04-07 16:34:58 -0400

    RScript for summary tables - Deliverable 2.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,7 +1,7 @@
 # importing library
 library(dplyr) 
 
-# reading csv file into dataframe
+# reading mechacar data csv file into dataframe
 mechacar_data <- read.csv(file='MechaCar_mpg.csv', check.names = F, stringsAsFactors = F)
 
 # creating multiple linear regression model
@@ -9,3 +9,14 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 # generating summary based on model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar_data))
+
+# reading suspension coil csv file into datafram
+suspensioncoil_data <- read.csv(file='Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+
+# creating summary table for PSI
+total_summary <- suspensioncoil_data %>% summarize(Mean=mean(PSI), Median=median(PSI), 
+                              Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+
+# creating lot summary table dependent on each manufacturing lot
+lot_summary <- suspensioncoil_data %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), 
+                              Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
